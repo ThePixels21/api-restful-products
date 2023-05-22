769969ee77254d35842d65ba3cb1c54467bdee30
ThePixels21 <santigamer521@gmail.com>
2023-05-22 12:02:11 -0500

    All services made without maker feature

--- src/main/java/products/products/controllers/ProductRestController.java
+++ src/main/java/products/products/controllers/ProductRestController.java
@@ -1,8 +1,30 @@
 package products.products.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+import products.products.model.Product;
+import products.products.service.IProductService;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1")
 public class ProductRestController {
+    
+    public IProductService productService;
+
+    @GetMapping("products")
+    @Transactional(readOnly = true)
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return productService.searchProduct();
+    }
     
 }
--- src/main/java/products/products/service/ProductServiceImpl.java
+++ src/main/java/products/products/service/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package products.products.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,11 +10,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import products.products.model.Product;
-
 import products.products.model.Category;
-import products.products.model.Product;
-import products.products.repository.ICategoryRepository;
+// import product.products.model.Maker;
 import products.products.repository.IProductRepository;
+import products.products.repository.ICategoryRepository;
+// import products.products.repository.IMakerRepository;
 import products.products.util.Util;
 
 @Service
@@ -40,6 +41,94 @@ public class ProductServiceImpl implements IProductService {
             } else {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<Product>> saveProduct(Product product, Long categoryId, Long makerId) {
+        try {
+            Optional<Category> category = categoryRepository.findById(categoryId);
+            if (category.isPresent()) {
+                product.setCategory(category.get());
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            /*
+            Optional<Maker> maker = makerRepository.findById(makerId);
+            if (maker.isPresent()) {
+                product.setMaker(maker.get());
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            */
+
+            Product productSaved = productRepository.save(product);
+
+            if(productSaved!=null) {
+                products.add(productSaved);
+            } else {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<Product>> updateProduct(Product product, Long categoryId, Long makerId, Long productId) {
+        try {
+            Optional<Category> category = categoryRepository.findById(categoryId);
+            if(category.isPresent()) {
+                product.setCategory(category.get());
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            /*
+            Optional<Maker> maker = makerRepository.findById(makerId);
+            if (maker.isPresent()) {
+                product.setMaker(maker.get());
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            */
+
+            Optional<Product> productSearch = productRepository.findById(productId);
+            if(productSearch.isPresent()) {
+                productSearch.get().setName(product.getName());
+                productSearch.get().setPrice(product.getPrice());
+                productSearch.get().setPicture(product.getPicture());
+                productSearch.get().setCategory(product.getCategory());
+                // productSearch.get().setMaker(product.getMaker());
+
+                Product productSaved = productRepository.save(productSearch.get());
+
+                if(productSaved!=null) {
+                    products.add(productSaved);
+                } else {
+                    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                }
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<Product>> deleteProduct(Long id) {
+        try {
+            productRepository.deleteById(id);
             return new ResponseEntity<>(products, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
@@ -48,27 +137,18 @@ public class ProductServiceImpl implements IProductService {
     }
 
     @Override
-    public ResponseEntity<List<Product>> saveProduct(Product product, Long categoryId, Long makerId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'saveProduct'");
-    }
-
-    @Override
-    public ResponseEntity<List<Product>> updateProduct(Product product, Long categoryId, Long makerId, Long productId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateProduct'");
-    }
-
-    @Override
-    public ResponseEntity<List<Product>> deleteProduct(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteProduct'");
-    }
-
-    @Override
     public ResponseEntity<Product> searchProductById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'searchProductById'");
+        try {
+            Optional<Product> product = productRepository.findById(id);
+            if(product.isPresent()) {
+                return new ResponseEntity<>(product.get(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     
 }
